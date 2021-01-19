d34ec536980b2bc40a718177c7308b47c00b769e
StackDoubleFlow <ojaslandge@gmail.com>
2021-01-19 16:50:10 -0600

    Remove version from build zip name

--- MappingExtensions/buildBMBF.sh
+++ MappingExtensions/buildBMBF.sh
@@ -6,8 +6,8 @@ buildScript="$NDKPath/build/ndk-build"
 
 ./$buildScript NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_APPLICATION_MK=./Application.mk
 mkdir BMBFBuild
-cp "./bmbfmod.json" "./libs/arm64-v8a/libmappingextensions.so" "./libs/arm64-v8a/libbeatsaber-hook_0_8_4.so" "./libs/arm64-v8a/libcodegen_0_4_0.so" ./BMBFBuild
+cp "./bmbfmod.json" "./libs/arm64-v8a/libmappingextensions.so" "./libs/arm64-v8a/libbeatsaber-hook_0_8_4.so" ./BMBFBuild
 cd BMBFBuild
-zip "../MappingExtensions_v0.18.0.zip" *
+zip "../MappingExtensions.zip" *
 cd ..
 rm -r BMBFBuild
